c4f9e8b014fe3c952be873ae3a3800a9a830b7ad
nutthakul <nutthakul.pr.61@ubu.ac.th>
2019-12-04 08:27:54 +0700

    500%

--- webapp/myapp/models.py
+++ webapp/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PrimeMinister(models.Model):
+    #fields
+    pid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length= 500)
+    bod = models.DateField()
+    startdate = models.DateField()
+    enddate = models.DateField()
+    imgurl = models.CharField(max_length= 500, default='')
+    party = models.CharField(max_length= 500, default='')
+    def __str__(self):
+        return f'{self.name} จากพรรค {self.party}'
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp import views 
+from myapp import views
 
 urlpatterns = [
     path('matmul',views.matmul),
